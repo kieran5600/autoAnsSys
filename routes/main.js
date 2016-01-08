ebbd89fb007fce4cc1b578a328fd95a0fe02f5e1
Kieran5600 <kieran5600@gmail.com>
2016-01-08 14:49:42 -0500

    first draft

--- routes/main.js
+++ routes/main.js
@@ -4,22 +4,29 @@ module.exports=function(app)
 	
 app.get('/',function(req,res){
 res.render('pages/index', {
-	author: 'Kieran Pierce',
+	author: 'Copyright Automated Answering Services 2016',
 	page: 'Home'
 	});
 });
 
 app.get('/about',function(req,res){
 res.render('pages/about',{
-	author: "Kieran Pierce",
-	page: 'About'
+	author: "Copyright Automated Answering Services 2016",
+	page: 'Company-Background'
 	});
 });
 
 app.get('/solutions',function(req,res){
 res.render('pages/solutions',{
-	author: "Kieran Pierce",
+	author: "Copyright Automated Answering Services 2016",
 	page: 'Solutions'
+	});
+});
+
+app.get('/contact',function(req,res){
+res.render('pages/contact',{
+	author: "Copyright Automated Answering Services 2016",
+	page: 'Contact'
 	});
 });
 
