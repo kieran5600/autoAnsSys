65afbe42bc145b9777a82773a49cdea5ef322e3d
Kieran5600 <kieran5600@gmail.com>
2016-01-12 18:52:46 -0500

    adding changes

--- routes/main.js
+++ routes/main.js
@@ -4,28 +4,28 @@ module.exports=function(app)
 	
 app.get('/',function(req,res){
 res.render('pages/index', {
-	author: 'Copyright Automated Answering Services 2016',
+	author: 'Automated Answering Services 2016',
 	page: 'Home'
 	});
 });
 
 app.get('/about',function(req,res){
 res.render('pages/about',{
-	author: "Copyright Automated Answering Services 2016",
+	author: "Automated Answering Services 2016",
 	page: 'Company-Background'
 	});
 });
 
 app.get('/solutions',function(req,res){
 res.render('pages/solutions',{
-	author: "Copyright Automated Answering Services 2016",
+	author: "Automated Answering Services 2016",
 	page: 'Solutions'
 	});
 });
 
 app.get('/contact',function(req,res){
 res.render('pages/contact',{
-	author: "Copyright Automated Answering Services 2016",
+	author: "Automated Answering Services 2016",
 	page: 'Contact'
 	});
 });
